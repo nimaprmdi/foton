32c9412c97aadda55c435d7ae46546bb50b75fd9
nimaprmdi <nimaprmdi@gmail.com>
2022-07-22 17:03:57 +0430

    Updates

---
+++ precache-manifest.83e337f695afe5db3dedc463f38d3324.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b8583ab7ef4270caa5aae6f2b8d617",
+    "url": "/foton/index.html"
+  },
+  {
+    "revision": "4d267c983bc0968994c6",
+    "url": "/foton/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "22eb5c03947bdd98d37e",
+    "url": "/foton/static/css/main.3f0fbe75.chunk.css"
+  },
+  {
+    "revision": "4d267c983bc0968994c6",
+    "url": "/foton/static/js/2.f3ee34e1.chunk.js"
+  },
+  {
+    "revision": "bac08d190ad2c3782685257760a40a06",
+    "url": "/foton/static/js/2.f3ee34e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22eb5c03947bdd98d37e",
+    "url": "/foton/static/js/main.7d6cfcdd.chunk.js"
+  },
+  {
+    "revision": "7b4ec90f95c9306eea51",
+    "url": "/foton/static/js/runtime-main.1844927e.js"
+  },
+  {
+    "revision": "e14493e6bd7e92e5dc329a1afc92fcd5",
+    "url": "/foton/static/media/about-us-bg1.e14493e6.jpg"
+  },
+  {
+    "revision": "4cad00d0ce3dd338acb3be41fae4a40f",
+    "url": "/foton/static/media/about-us.4cad00d0.png"
+  },
+  {
+    "revision": "43b8a23cd6d30934e1a80638f2c9ca4d",
+    "url": "/foton/static/media/cloud.43b8a23c.png"
+  },
+  {
+    "revision": "595413d57ca74e1b27e76d4220e181ea",
+    "url": "/foton/static/media/coins.595413d5.png"
+  },
+  {
+    "revision": "b07caadc92f54dd1087c7ccbbacdbf68",
+    "url": "/foton/static/media/container-bg.b07caadc.jpg"
+  },
+  {
+    "revision": "dc2e15cd1ee6b0fc480b685012ad9447",
+    "url": "/foton/static/media/laptop.dc2e15cd.png"
+  },
+  {
+    "revision": "6ca2c45100623e6051bc999ad540ed0e",
+    "url": "/foton/static/media/layer-blue-bottom.6ca2c451.png"
+  },
+  {
+    "revision": "617ebeb7aec12eca0b0ba1b6d036ead5",
+    "url": "/foton/static/media/layer-blue.617ebeb7.png"
+  },
+  {
+    "revision": "99986a2285bae153d1144146d1a50517",
+    "url": "/foton/static/media/layer-cream-orange.99986a22.png"
+  },
+  {
+    "revision": "1ab13b3e1e0571f077b3cc762f03c43d",
+    "url": "/foton/static/media/layer-cream.1ab13b3e.png"
+  },
+  {
+    "revision": "6eb5f74148c3f5de503bdc5d0cd16f0f",
+    "url": "/foton/static/media/layer-orange-right.6eb5f741.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/foton/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "2178e98f6f0c240afc1c8da4f9cd6fd8",
+    "url": "/foton/static/media/man.2178e98f.png"
+  },
+  {
+    "revision": "3ab52653d1e96337254bc4af11a81056",
+    "url": "/foton/static/media/message.3ab52653.png"
+  },
+  {
+    "revision": "4e0212fb61c61fe27db77b9c3d00a11b",
+    "url": "/foton/static/media/mobile-robot.4e0212fb.png"
+  },
+  {
+    "revision": "7d638e70326c927e528ed0b2d2c981a8",
+    "url": "/foton/static/media/person.7d638e70.png"
+  },
+  {
+    "revision": "524aa53a2a6467155d90e7f8f7e980a1",
+    "url": "/foton/static/media/secure.524aa53a.png"
+  },
+  {
+    "revision": "20a7bbc38a0721f2087a5ff207afce4d",
+    "url": "/foton/static/media/servers.20a7bbc3.png"
+  },
+  {
+    "revision": "c052eb1550395b72fe34e31995ec77eb",
+    "url": "/foton/static/media/shell.c052eb15.png"
+  },
+  {
+    "revision": "71af71fc2caa6d95462ff9b47152bb2b",
+    "url": "/foton/static/media/site.71af71fc.png"
+  },
+  {
+    "revision": "8b8b343cd4ad2f889f859efcf8232f9a",
+    "url": "/foton/static/media/wallet.8b8b343c.png"
+  },
+  {
+    "revision": "46e7030bd9a2371137d01b986eca70f2",
+    "url": "/foton/static/media/wifi.46e7030b.png"
+  }
+]);
